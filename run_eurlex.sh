5902fbc5fe0a6c97fd420229da8043f71d274377
nilesh2797 <nileshgupta2797@gmail.com>
2021-09-22 14:44:53 -0500

    cleaned

--- run_eurlex.sh
+++ run_eurlex.sh
@@ -1,2 +1,2 @@
 extra_args="${@:2}"
-./run.sh GZ-Eurlex-4.3K $1 -bilinear_classifier_cost 5 -bs_count 120 -bs_direct_wt 0.8 -bs_alpha 0.02 -shortyK 150 -bilinear_shortyK 150 -score_alpha 0.9 -num_thread 1 ${extra_args}
+./run.sh GZ-Eurlex-4.3K $1 -bilinear_classifier_cost 5 -bs_count 120 -bs_direct_wt 0.8 -bs_alpha 0.02 -shortyK 150 ${extra_args}
