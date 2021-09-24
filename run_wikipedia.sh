2c340e1880ff5ee02a95c627d1a5d16e1aff5a68
Nilesh Gupta <nileshgupta2797@gmail.com>
2021-09-24 17:21:48 -0500

    Update run_wikipedia.sh

--- run_wikipedia.sh
+++ run_wikipedia.sh
@@ -1,2 +1,2 @@
 extra_args="${@:2}"
-./run.sh GZ-Wikipedia-1M $1 -bilinear_classifier_cost 2 -bs_count 40 -bs_direct_wt 0.1 -bs_alpha 0.01 ${extra_args}
+./run.sh GZ-Wikipedia-1M $1 -bilinear_classifier_cost 2 -bs_count 40 -bs_direct_wt 0.1 -bs_alpha 0.01 -score_alpha 0.8 ${extra_args}
